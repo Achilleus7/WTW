d8bea4b52f369891d681cd454960689aa258fa44
Stephanie Graessle <34826134+sgraessle27@users.noreply.github.com>
2020-12-06 02:13:55 -0600

    Update createaccount.js

--- WTW/src/main/resources/static/createaccount.js
+++ WTW/src/main/resources/static/createaccount.js
@@ -1,21 +1,31 @@
+import bcrypt from 'bcrypt';
+
 function addUser()
 {
-
-    var user=document.getElementById("uname").value;
-    var password=document.getElementById("pass").value;
-    var zip = document.getElementById("zip").value;
-
-    var shortsRec = document.getElementById("shorts").value;
-    var shirtRec = document.getElementById("shirt").value;
-    var jacketRec = document.getElementById("jacket").value;
-    var coatRec = document.getElementById("coat").value;
-    const data = {
-        username: user,
-        password: password,
-        zipCode: zip,
-        jacketThreshold: jacketRec,
-        coatThreshold: coatRec
-    };
+    const user = document.getElementById("username").value;
+	
+	const bcrypt = require('bcrypt');
+	const saltRounds = 10;
+	const password = document.getElementById("password").value;
+	
+    const zip = document.getElementById("zip").value;
+	
+    const shortsRec = document.getElementById("shorts").value;
+    const shirtRec = document.getElementById("shirt").value;
+    const jacketRec = document.getElementById("jacket").value;
+    const coatRec = document.getElementById("coat").value;
+	
+	//password hashing
+	bcrypt.hash(password, saltRounds, (err, hash) ==> {
+		const data = {
+			username: user,
+			password: password,
+			zipCode: zip,
+			jacketThreshold: jacketRec,
+			coatThreshold: coatRec
+		};
+	});
+	
     fetch('http://localhost:8080/newuser', {
         method: 'POST', // or 'PUT'
         headers: {
