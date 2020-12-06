d98fb647ebc660e4ad518f829a250be4d55307f0
Stephanie Graessle <34826134+sgraessle27@users.noreply.github.com>
2020-12-06 02:17:50 -0600

    Update login.js
    Added password hashing

--- WTW/src/main/resources/static/login.js
+++ WTW/src/main/resources/static/login.js
@@ -1,12 +1,16 @@
+import bcrypt from 'bcrypt';
+
 function validate()
 {
 	//these are the username and passwords inputted by the user
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
+    const username=document.getElementById("username").value;
+    const password=document.getElementById("password").value;
+	
+	localStorage["user"] = username;
 	
 	//search up username in database and check whether the password matches the one in the database
 	
-	//replace "admin" and "user" here with those fetched from the database
+	//replace "admin" with username from database
 	/*if (username does not exist in database)
 	{
 		alert("Log in failed. Please try again!");
@@ -21,4 +25,25 @@ function validate()
     else{
         alert("Log in failed. Please try again!");
     }
+	
+	//when done testing with "admin" and "user", uncomment this section to enable password hashing
+	/*
+	//"username" should be replaced with the user's password from the database
+	if ("username" does not exist in database)
+	{
+		alert("Log in failed. Please try again!");
+	}
+	else{
+		bcrypt.compare(password, "username", function(err, result) {
+			if (result == true) {
+				alert("Logged in successfully!");
+			return false;
+			}
+			else {
+				alert("Log in failed. Please try again!");
+			}
+			
+		});
+	}
+	*/
 }
